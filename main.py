bbce7fbcf72b7ce2df12d3b248c0361e53f373f3
Sreeshant Nair <your-email@example.com>
2026-06-26 00:36:34 +0530

    MalayalamLang initial version

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+from keywords_malayalam import KEYWORDS as MALAYALAM_KEYWORDS
+from keywords_latin import KEYWORDS as LATIN_KEYWORDS
+
 from translator import translate_code
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,36 +17,73 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def malayalam_print(value):
-    translations = {
-        True: "സത്യം",
-        False: "അസത്യം",
-        None: "ഒന്നുമില്ല"
-    }
-    return str(translations.get(value, value))
+
+def convert_output(value, mode):
+
+    if mode == "latin":
+
+        translations = {
+            True: "sathyam",
+            False: "asathyam",
+            None: "onnumilla"
+        }
+
+    else:
+
+        translations = {
+            True: "സത്യം",
+            False: "അസത്യം",
+            None: "ഒന്നുമില്ല"
+        }
+
+    return str(
+        translations.get(
+            value,
+            value
+        )
+    )
 
 
 class CodeRequest(BaseModel):
     code: str
+    mode: str
 
 
 @app.post("/run")
 def run_code(data: CodeRequest):
 
-    python_code = translate_code(data.code)
+    # Select keyword dictionary
+    if data.mode == "latin":
+        keywords = LATIN_KEYWORDS
+    else:
+        keywords = MALAYALAM_KEYWORDS
+
+    # Translate MalayalamLang / Roman Malayalam
+    # into Python
+    python_code = translate_code(
+        data.code,
+        keywords
+    )
 
     output = []
 
     def capture_print(value):
+
         output.append(
-            malayalam_print(value)
+            convert_output(
+                value,
+                data.mode
+            )
         )
 
     exec_globals = {
         "malayalam_print": capture_print
     }
 
-    exec(python_code, exec_globals)
+    exec(
+        python_code,
+        exec_globals
+    )
 
     return {
         "python_code": python_code,
--- translator.py
+++ translator.py
@@ -1,13 +1,13 @@
-from keywords import KEYWORDS
 
-def translate_code(code):
+def translate_code(code, keywords):
 
     translated = code
 
-    for malayalam_word, python_word in KEYWORDS.items():
+    for key, value in keywords.items():
+
         translated = translated.replace(
-            malayalam_word,
-            python_word
+            key,
+            value
         )
 
     return translated
